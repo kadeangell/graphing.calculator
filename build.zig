f2bc9e02f6ca8cc57f27162d640c4e62a0d68907
Kade Angell <kade@angell.fyi>
2025-12-15 11:41:40 -0700

    Added app build export

--- build.zig
+++ build.zig
@@ -68,4 +68,63 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
     test_step.dependOn(&run_equation_tests.step);
+
+    // macOS App Bundle
+    const app_step = b.step("app", "Build macOS .app bundle");
+
+    const app_name = "Graphing Engine";
+    const bundle_id = "com.graphing.engine";
+
+    // Create app bundle structure
+    const app_dir = b.fmt("{s}.app", .{app_name});
+    const contents_dir = b.fmt("{s}/Contents", .{app_dir});
+    const macos_dir = b.fmt("{s}/MacOS", .{contents_dir});
+    const resources_dir = b.fmt("{s}/Resources", .{contents_dir});
+
+    // Create directories
+    const mkdir_app = b.addSystemCommand(&[_][]const u8{"mkdir", "-p", macos_dir});
+    const mkdir_resources = b.addSystemCommand(&[_][]const u8{"mkdir", "-p", resources_dir});
+
+    // Copy executable
+    const copy_exe = b.addSystemCommand(&[_][]const u8{
+        "cp",
+        b.getInstallPath(.bin, exe.name),
+        b.fmt("{s}/{s}", .{macos_dir, app_name}),
+    });
+    copy_exe.step.dependOn(b.getInstallStep());
+    copy_exe.step.dependOn(&mkdir_app.step);
+
+    // Create Info.plist
+    const plist_content = b.fmt(
+        \\<?xml version="1.0" encoding="UTF-8"?>
+        \\<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        \\<plist version="1.0">
+        \\<dict>
+        \\    <key>CFBundleName</key>
+        \\    <string>{s}</string>
+        \\    <key>CFBundleDisplayName</key>
+        \\    <string>{s}</string>
+        \\    <key>CFBundleIdentifier</key>
+        \\    <string>{s}</string>
+        \\    <key>CFBundleVersion</key>
+        \\    <string>1.0.0</string>
+        \\    <key>CFBundlePackageType</key>
+        \\    <string>APPL</string>
+        \\    <key>CFBundleExecutable</key>
+        \\    <string>{s}</string>
+        \\    <key>LSMinimumSystemVersion</key>
+        \\    <string>11.0</string>
+        \\    <key>NSHighResolutionCapable</key>
+        \\    <true/>
+        \\</dict>
+        \\</plist>
+        \\
+    , .{app_name, app_name, bundle_id, app_name});
+
+    const write_plist = b.addWriteFile(b.fmt("{s}/Info.plist", .{contents_dir}), plist_content);
+    write_plist.step.dependOn(&mkdir_app.step);
+
+    app_step.dependOn(&copy_exe.step);
+    app_step.dependOn(&write_plist.step);
+    app_step.dependOn(&mkdir_resources.step);
 }
